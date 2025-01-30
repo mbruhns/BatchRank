daa712f784dc381afb7e945a60bf216d0e06eb3b
Matthias Bruhns <matthias.bruhns@uni-tuebingen.de>
2025-01-30 17:43:02 +0100

    Naive design.

--- src/BatchRank/main.py
+++ src/BatchRank/main.py
@@ -7,7 +7,8 @@ from sklearn.model_selection import cross_val_score
 
 def batch_scoring(
     X: pd.DataFrame,
-    y,
+    data_columns,
+    label_columns,
     clf=HistGradientBoostingClassifier(),
     scoring=balanced_accuracy_score,
     cv=5,
@@ -15,15 +16,15 @@ def batch_scoring(
     score_lst = []
     label_lst = []
 
-    for label in np.unique(y):
-        cv_score = cross_val_score(clf, X.reshape(-1, 1), label, cv=cv)
-        score_lst.append(np.median(cv_score))
-
+    for label in np.unique(label_columns):
+        cv_score = cross_val_score(
+            clf, X[data_columns].values, X[label].values, cv=cv
+        )
         score_lst.append(np.median(cv_score))
         label_lst.append(label)
 
     df = pd.DataFrame({"Label": label_lst, "Score": score_lst})
-    df = df.sort_values("Score")
+    df = df.sort_values("Score", ascending=False)
 
     return df
 
@@ -32,8 +33,16 @@ def main():
     rng = np.random.default_rng(42)
 
     X = rng.random(size=(1000, 3))
-    print(X)
-    y = rng.integers(0, 2, size=1000)
+    y = rng.integers(0, 2, size=(1000, 2))
+
+    test_df = pd.DataFrame(data=X)
+    test_df["A"] = y[:, 0]
+    test_df["B"] = y[:, 1]
+
+    result = batch_scoring(
+        X=test_df, data_columns=[0, 1, 2], label_columns=["A", "B"]
+    )
+    print(result)
 
 
 if __name__ == "__main__":
