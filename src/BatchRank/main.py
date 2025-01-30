a1abab53414a2b84a718e0e2c1e571e686ffae20
Matthias Bruhns <matthias.bruhns@uni-tuebingen.de>
2025-01-30 21:10:22 +0100

    Add function for anndata objects.

--- src/BatchRank/main.py
+++ src/BatchRank/main.py
@@ -1,25 +1,33 @@
+from typing import Optional, Union
+
 import numpy as np
 import pandas as pd
+from anndata import AnnData
+from numpy.typing import ArrayLike
 from sklearn.ensemble import HistGradientBoostingClassifier
-from sklearn.metrics import balanced_accuracy_score
 from sklearn.model_selection import cross_val_score
 
 
 def batch_scoring(
-    X: pd.DataFrame,
-    data_columns,
-    label_columns,
+    dframe: pd.DataFrame,
+    data_columns: ArrayLike,
+    label_columns: ArrayLike,
     clf=HistGradientBoostingClassifier(),
-    scoring=balanced_accuracy_score,
-    cv=5,
-):
+    scoring="matthews_corrcoef",
+    cv: int = 5,
+) -> pd.DataFrame:
     score_lst = []
     label_lst = []
 
     for label in np.unique(label_columns):
         cv_score = cross_val_score(
-            clf, X[data_columns].values, X[label].values, cv=cv
+            estimator=clf,
+            X=dframe[data_columns].values,
+            y=dframe[label].values,
+            scoring=scoring,
+            cv=cv,
         )
+
         score_lst.append(np.median(cv_score))
         label_lst.append(label)
 
@@ -29,18 +37,37 @@ def batch_scoring(
     return df
 
 
+def batch_scoring_anndata(
+    adata: AnnData,
+    data_columns: Union[str, int],
+    label_columns: Union[str],
+    scoring: Optional[str] = "matthews_corrcoef",
+    cv: Optional[int] = 5,
+    inplace: bool = True,
+) -> Optional[AnnData]:
+    X = adata.X
+    y = adata.obs[label_columns].values
+
+    # Todo: Implement the actual scoring
+
+    if not inplace:
+        return adata
+
+
 def main():
     rng = np.random.default_rng(42)
 
-    X = rng.random(size=(1000, 3))
-    y = rng.integers(0, 2, size=(1000, 2))
+    n_samples = 500
+    X = rng.random(size=(n_samples, 3))
+    pat = rng.integers(0, 5, size=(n_samples, 1))
+    lab = rng.integers(0, 3, size=(n_samples, 1))
 
     test_df = pd.DataFrame(data=X)
-    test_df["A"] = y[:, 0]
-    test_df["B"] = y[:, 1]
+    test_df["Patient"] = pat
+    test_df["Lab"] = lab
 
     result = batch_scoring(
-        X=test_df, data_columns=[0, 1, 2], label_columns=["A", "B"]
+        dframe=test_df, data_columns=[0, 1, 2], label_columns=["Patient", "Lab"]
     )
     print(result)
 
